661ee4e24d07aa766a1519e65926a54edd458f08
Jaidil14 <jaidilrachmad@gmail.com>
2024-05-10 15:22:19 +0700

    Menambahkan void peek

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -41,4 +41,9 @@ public:
 		top = top->next; // Update the top pointer to the next node
 
 	}
+
+// Peek/Top operation: Retrieve the value of the element without removing it
+	void peek() {
+
+	}
 };
