1ed26032511e2edcdc2ee8a7da75fc7318d0cafa
Jaidil14 <jaidilrachmad@gmail.com>
2024-05-10 15:54:00 +0700

    Menghapus comments

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-// Node class representing a single node in the linked list
+
 class Node {
 public:
 	int data;
@@ -12,37 +12,37 @@ public:
 	}
 };
 
-// Stack class
+
 class Stack {
 private:
-	Node* top; // Pointer to the top node of the stack
+	Node* top; 
 
 public:
 	Stack() {
-		top = NULL; // Initialize the stack with a null top pointer
+		top = NULL; 
 	}
 
-// Push operation: Insert an element onto the top of the stack
+
 	int push(int value) {
-		Node* newNode = new Node(); // 1.Allocate memory for the new node
-		newNode->data = value; // 2. Assign value
-		newNode->next = top; // 3. Set the next pointer of the new node to the current top node
-		top = newNode; // 4. Update the top pointer to the new node
+		Node* newNode = new Node(); 
+		newNode->data = value;
+		newNode->next = top; 
+		top = newNode; 
 		cout << "Push value: " << value << endl;
 		return value;
 	}
 
-// Pop operation: Remove the topmost element from the stack
+
 	void pop() {
 		if (isEmpty()) {
 			cout << "Stack is empty." << endl;
 		}
 		cout << "Popped value: " << top->data << endl;
-		top = top->next; // Update the top pointer to the next node
+		top = top->next; 
 
 	}
 
-// Peek/Top operation: Retrieve the value of the element without removing it
+
 	void peek() {
 		if (top == NULL) {
 			cout << "List is empty." << endl;
@@ -54,12 +54,12 @@ public:
 				current = current->next;
 			}
 			cout << endl;
-		}// Return the value of the top node
+		}
 	}
 
-// IsEmpty operation: Check if the stack is empty
+
 	bool isEmpty() {
-		return top == NULL; // Return true if the top pointer is NULL, indicating an empty stack
+		return top == NULL; 
 	}
 
 };
@@ -108,6 +108,8 @@ int main() {
 			break;
 		}
 		
+		cout << endl;
 	}
-	
+
+	return 0;
 }
